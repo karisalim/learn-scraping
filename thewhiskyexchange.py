895657041924f5fbd908e02b81e7b3b32d3088e1
karisalim <karimsalim33000@gmail.com>
2026-01-01 23:01:48 +0200

    feat: Add HTTP request with custom User-Agent for The Whisky Exchange

--- thewhiskyexchange.py
+++ thewhiskyexchange.py
@@ -1,4 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.thewhiskyexchange.com/c/35/japanese-whisky"
+URL = "https://www.thewhiskyexchange.com/"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (BeOS; U; Haiku BePC; en-US; rv:1.8.1.17) Gecko/20080831 BonEcho/2.0.0.17"
+}
+
+r = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky', headers=headers)
